b99069096e54f81166abfebeb2283ee41f7d1be0
stevegu <fulcrum1204@gmail.com>
2026-08-03 15:36:21 +0800

    bug fixed

--- prefabs/status_bar/status_bar.gd
+++ prefabs/status_bar/status_bar.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+var _version_number: String = ""
+
 @export var version_number: String : 
 	set(value):
-		version_label.text = version_number
+		_version_number = value
+		if version_label:
+			version_label.text = value
 	get:
-		return version_number
+		return _version_number
 
 
 @onready var version_label: Label = $Label
+
+func _ready() -> void:
+	version_label.text = _version_number
