00c487ada1649824f6983a5f7059296e7cd014a5
stevegu <fulcrum1204@gmail.com>
2026-08-10 14:44:13 +0800

    building data updated

--- scripts/data/building_data.gd
+++ scripts/data/building_data.gd
@@ -1,2 +1,9 @@
 extends BaseData
 class_name BuildingData
+
+@export var position: Vector2i
+@export var unlockLevel: int
+@export var buildingCost: int
+
+@export var is_unlocked: bool
+@export var is_working: bool
