5678bf39ad335a24bfb5cce99f1972f5596e4e8a
stevegu <fulcrum1204@gmail.com>
2026-09-11 21:43:21 +0800

    update loading scene

--- scenes/scenario_loading_scene/scenario_loading_scene.gd
+++ scenes/scenario_loading_scene/scenario_loading_scene.gd
@@ -6,6 +6,9 @@ extends Control
 @onready var bar := $VBoxContainer/ColorRect/ProgressBarRect
 @onready var bar_mat := bar.material as ShaderMaterial
 
+@onready var start_button: TextureButton = $VBoxContainer/ColorRect/StartButton
+@onready var words_label: Label = $VBoxContainer/ColorRect/WordsLabel
+
 var scenario_data: ScenarioData
 
 # 假设这是你的加载进度（0.0 ~ 1.0）
@@ -19,6 +22,8 @@ func _ready() -> void:
 	if scenario_data.loading_texture != null:
 		loading_texture.texture = scenario_data.loading_texture
 
+	start_button.visible = false
+	start_button.pressed.connect(_on_start_button_pressed)
 	ResourceLoader.load_threaded_request(scene_path)
 
 
@@ -33,8 +38,14 @@ func _process(_delta: float) -> void:
 	# if status == ResourceLoader.THREAD_LOAD_LOADED:
 		# get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene_path))
 	
-	load_progress = load_progress + 0.05 * _delta
+	load_progress = load_progress + 0.1 * _delta
 	
 	if load_progress < 1.0:
 		mat.set_shader_parameter("progress", load_progress)
 		bar_mat.set_shader_parameter("progress", load_progress)
+	else:
+		start_button.visible = true
+		words_label.visible = false
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene_path))
